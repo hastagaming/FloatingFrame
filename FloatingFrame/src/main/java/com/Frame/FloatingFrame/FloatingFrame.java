3220ec6a868a89bab7ea1da3ed0d9b01f5bd672c
hastagaming <rip.hastagaming@gmail.com>
2026-04-22 18:24:04 +0700

    Setup gh-pages untuk library

--- FloatingFrame/src/main/java/com/Frame/FloatingFrame/FloatingFrame.java
+++
@@ -1,9 +0,0 @@
-package com.Frame.FloatingFrame;
-import android.content.Context;
-import android.view.View;
-
-public class FloatingFrame {
-    public static FloatingFrameInstance with(Context context) {
-        return new FloatingFrameInstance(context);
-    }
-}
